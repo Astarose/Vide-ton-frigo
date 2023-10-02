08da7f4095628e87b0351b65cb8b11035fc168b5
MartinPAVY <113358964+MartinPAVY@users.noreply.github.com>
2023-10-02 14:18:40 +0200

    default project

---
+++ vide-ton-friog/src/App.js
@@ -0,0 +1,12 @@
+//App.js
+
+import './App.css';
+import Home from "./components/Home.tsx";
+
+function App() {
+  return (
+      <Home/>
+  );
+}
+
+export default App;
---
+++ vide-ton-friog/src/components/Home.tsx
@@ -0,0 +1,13 @@
+//Home.tsx
+
+function Home() {
+    return(
+        <div className="homePage">
+            <p>
+                Coucou
+            </p>
+        </div>
+    )
+}
+
+export default Home;
