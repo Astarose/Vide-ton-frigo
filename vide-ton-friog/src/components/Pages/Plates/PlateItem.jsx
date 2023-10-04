ce87ec09449e68c2d0356cb2f0efc7c2eb1926ab
MartinPAVY <113358964+MartinPAVY@users.noreply.github.com>
2023-10-04 17:08:53 +0200

    création et intégration footer et création homePage sans carroussel sur le homepage

--- vide-ton-friog/src/components/Pages/Plates/PlateItem.jsx
+++ vide-ton-friog/src/components/Pages/Plates/PlateItem.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Swal from "sweetalert2";
 
 export function PlateItem({item, addToCart}){
+
+
     
     const add = () => {
         Swal.fire((
@@ -19,7 +21,7 @@ export function PlateItem({item, addToCart}){
       
     }
     return(
-        <article className="cartItem">
+        <article className="plateItem">
             <img
                 src="https://i.guim.co.uk/img/media/63de40b99577af9b867a9c57555a432632ba760b/0_266_5616_3370/master/5616.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=93458bbe24b9f88451ea08197888ab8e"
                 id="picture"
