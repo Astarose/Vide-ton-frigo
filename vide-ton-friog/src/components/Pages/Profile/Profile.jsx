9ebb3fe57da94b4bfea9a4a55398870275756b31
MartinPAVY <113358964+MartinPAVY@users.noreply.github.com>
2023-10-05 16:53:09 +0200

    patch 2

--- vide-ton-friog/src/components/Pages/Profile/Profile.jsx
+++ vide-ton-friog/src/components/Pages/Profile/Profile.jsx
@@ -1,17 +1,10 @@
 //Profile.tsx
-import { Link } from "react-router-dom";
 
 
 function Profile() {
     return(
         <div className="homePage">
             <p>Coucou de profil</p>
-            <Link to="/inscription">
-            <button className="cda">S'inscrire</button>
-          </Link>
-          <Link to="/connexion">
-            <button className="cda">Se connecter</button>
-          </Link>
         </div>
     )
 }
